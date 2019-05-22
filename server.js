729bf9d1d118210e99fe5dc9ea6935983bb616e2
Nash McDonald <nash42192@gmail.com>
2019-05-22 13:29:03 -0700

    added get route for pdxpipeline

--- server.js
+++ server.js
@@ -31,21 +31,21 @@ mongoose.connect("mongodb://localhost/skraperdb", { useNewUrlParser: true });
 
 // Routes
 
-// A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+// A GET route for scraping the events12 website
+app.get("/scrape12", function (req, res) {
   // First, we grab the body of the html with axios
-  axios.get("https://www.events12.com/portland/").then(function(response) {
+  axios.get("https://www.events12.com/portland/").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an Event tag, and do the following:
-    $("article").each(function(i, element) {
+    $("article").each(function (i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-      .children("h3")
+        .children("h3")
         .text();
       result.event_date = $(this)
         .children("div.date")
@@ -53,15 +53,15 @@ app.get("/scrape", function(req, res) {
       result.link = $(this)
         .children("a")
         .attr("href");
-        
+
 
       // Create a new Event using the `result` object built from scraping
       db.Event.create(result)
-        .then(function(dbEvent) {
+        .then(function (dbEvent) {
           // View the added result in the console
           console.log(dbEvent);
         })
-        .catch(function(err) {
+        .catch(function (err) {
           // If an error occurred, log it
           console.log(err);
         });
@@ -72,15 +72,54 @@ app.get("/scrape", function(req, res) {
   });
 });
 
+// A GET route for scraping the pdxpipeline website
+app.get("/scrapePipe", function (req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.pdxpipeline.com/events/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+    //console.log(response.data);
+
+    $("div.event-details").each(function(i, element){
+      var result = {};
+      result.title = $(this)
+        .children("h2")
+        .children("a")
+        .attr("data-tooltip");
+      result.event_date = $(this)
+        .children("div.tribe-events-event-meta")
+        .children("div.author")
+        .children("div.tribe-single-event-details")
+        .children("span.tribe-event-date-start")
+        .text();
+      result.link = $(this)
+        .children("h2")
+        .children("a")
+        .attr("href");
+      result.img  =$(this)
+        .parent()
+        .children("div.event-thumb")
+        .children("a")
+        .children("img")
+        .attr("src");
+        
+
+      console.log(result);
+    })
+    res.send("scraped");
+  });
+
+});
+
 // Route for getting all Events from the db
-app.get("/events", function(req, res) {
+app.get("/events", function (req, res) {
   // Grab every document in the Events collection
   db.Event.find({})
-    .then(function(dbEvent) {
+    .then(function (dbEvent) {
       // If we were able to successfully find Events, send them back to the client
       res.json(dbEvent);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
@@ -123,6 +162,6 @@ app.get("/events", function(req, res) {
 // });
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
