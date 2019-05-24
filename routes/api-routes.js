b8d7184d3f5f35a5d4d708045ca98e17ad5b6a81
Nash McDonald <nash42192@gmail.com>
2019-05-23 17:22:59 -0700

    added html and api routes to their own folder and files

---
+++ routes/api-routes.js
@@ -0,0 +1,93 @@
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("../models");
+
+module.exports = function (app) {
+
+    // A GET route for scraping the pdxpipeline website
+    app.get("/scrapePipe", function (req, res) {
+        // First, we grab the body of the html with axios
+        axios.get("https://www.pdxpipeline.com/events/").then(function (response) {
+            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            const $ = cheerio.load(response.data);
+            //console.log(response.data);
+
+            $("div.event-details").each(function (i, element) {
+                var result = {};
+                result.title = $(this)
+                    .children("h2")
+                    .children("a")
+                    .attr("data-tooltip");
+                result.event_date = $(this)
+                    .children("div.tribe-events-event-meta")
+                    .children("div.author")
+                    .children("div.tribe-single-event-details")
+                    .children("span.tribe-event-date-start")
+                    .text();
+                result.link = $(this)
+                    .children("h2")
+                    .children("a")
+                    .attr("href");
+                result.img = $(this)
+                    .parent()
+                    .children("div.event-thumb")
+                    .children("a")
+                    .children("img")
+                    .attr("src");
+
+                // Create a new Event using the `result` object built from scraping
+                db.Event.create(result)
+                    .then(function (dbEvent) {
+                        // View the added result in the console
+                        console.log(dbEvent);
+                    })
+                    .catch(function (err) {
+                        // If an error occurred, log it
+                        console.log(err);
+                    });
+            });
+        });
+    });   
+
+    // A GET route for scraping the events12 website
+app.get("/scrape12", function (req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("https://www.events12.com/portland/").then(function (response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      const $ = cheerio.load(response.data);
+  
+      // Now, we grab every h2 within an Event tag, and do the following:
+      $("article").each(function (i, element) {
+        // Save an empty result object
+        var result = {};
+  
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children("h3")
+          .text();
+        result.event_date = $(this)
+          .children("div.date")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+  
+  
+        // Create a new Event using the `result` object built from scraping
+        db.Event.create(result)
+          .then(function (dbEvent) {
+            // View the added result in the console
+            console.log(dbEvent);
+          })
+          .catch(function (err) {
+            // If an error occurred, log it
+            console.log(err);
+          });
+      });
+  
+      // Send a message to the client
+      res.send("Scrape Complete");
+    });
+  });
+}
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 // Require all models
-var db = require("./models");
+const db = require("./models");
 
-var PORT = 3000;
+const PORT = 3000;
 
 // Initialize Express
-var app = express();
+const app = express();
 
 // Configure middleware
 
@@ -29,137 +29,14 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/skraperdb", { useNewUrlParser: true });
 
-// Routes
+// Set Handlebars.
+const exphbs = require("express-handlebars");
 
-// A GET route for scraping the events12 website
-app.get("/scrape12", function (req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://www.events12.com/portland/").then(function (response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-    // Now, we grab every h2 within an Event tag, and do the following:
-    $("article").each(function (i, element) {
-      // Save an empty result object
-      var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("h3")
-        .text();
-      result.event_date = $(this)
-        .children("div.date")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-
-      // Create a new Event using the `result` object built from scraping
-      db.Event.create(result)
-        .then(function (dbEvent) {
-          // View the added result in the console
-          console.log(dbEvent);
-        })
-        .catch(function (err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
-    });
-
-    // Send a message to the client
-    res.send("Scrape Complete");
-  });
-});
-
-// A GET route for scraping the pdxpipeline website
-app.get("/scrapePipe", function (req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://www.pdxpipeline.com/events/").then(function (response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-    //console.log(response.data);
-
-    $("div.event-details").each(function(i, element){
-      var result = {};
-      result.title = $(this)
-        .children("h2")
-        .children("a")
-        .attr("data-tooltip");
-      result.event_date = $(this)
-        .children("div.tribe-events-event-meta")
-        .children("div.author")
-        .children("div.tribe-single-event-details")
-        .children("span.tribe-event-date-start")
-        .text();
-      result.link = $(this)
-        .children("h2")
-        .children("a")
-        .attr("href");
-      result.img  =$(this)
-        .parent()
-        .children("div.event-thumb")
-        .children("a")
-        .children("img")
-        .attr("src");
-        
-
-      console.log(result);
-    })
-    res.send("scraped");
-  });
-
-});
-
-// Route for getting all Events from the db
-app.get("/events", function (req, res) {
-  // Grab every document in the Events collection
-  db.Event.find({})
-    .then(function (dbEvent) {
-      // If we were able to successfully find Events, send them back to the client
-      res.json(dbEvent);
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
-// // Route for grabbing a specific Event by id, populate it with it's note
-// app.get("/Events/:id", function(req, res) {
-//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//   db.Event.findOne({ _id: req.params.id })
-//     // ..and populate all of the notes associated with it
-//     .populate("note")
-//     .then(function(dbEvent) {
-//       // If we were able to successfully find an Event with the given id, send it back to the client
-//       res.json(dbEvent);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
-
-// // Route for saving/updating an Event's associated Note
-// app.post("/Events/:id", function(req, res) {
-//   // Create a new note and pass the req.body to the entry
-//   db.Note.create(req.body)
-//     .then(function(dbNote) {
-//       // If a Note was created successfully, find one Event with an `_id` equal to `req.params.id`. Update the Event to be associated with the new Note
-//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-//       return db.Event.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-//     })
-//     .then(function(dbEvent) {
-//       // If we were able to successfully update an Event, send it back to the client
-//       res.json(dbEvent);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
 // Start the server
 app.listen(PORT, function () {
