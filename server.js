def806b63963b5709b1060289c15f8299e26d81a
Nash McDonald <nash42192@gmail.com>
2019-05-25 15:26:13 -0700

    testing connecting to hosted db

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static("public"));
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/skraperdb";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 // Set Handlebars.
 const exphbs = require("express-handlebars");
